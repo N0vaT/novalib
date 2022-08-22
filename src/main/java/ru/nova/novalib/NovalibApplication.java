58d520068278a3f0bfa9e22be6e0671f0b31b7d2
N0vaT <Novikovto92@yandex.ru>
2022-08-22 07:37:16 +0300

    Init project

---
+++ src/main/java/ru/nova/novalib/NovalibApplication.java
@@ -0,0 +1,13 @@
+package ru.nova.novalib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovalibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovalibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nova/novalib/NovalibApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nova.novalib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovalibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
